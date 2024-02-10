fb3a2db1c65267196e8dd36cb0e0f44feb17977f
Siddhant Singh <singhsiddhantakshat@gmail.com>
2024-02-10 09:15:12 -0600

    finish learning this teod today.

--- react-router-tutorial--/src/main.jsx
+++ react-router-tutorial--/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contacts";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "contacts/:contactId",
+				element: <Contact />,
+			},
+		],
+	},
+	{
+		path: "contacts/:contactId",
+		element: <Contact />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
